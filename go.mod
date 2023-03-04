a5b49680694092256a3c168e30663a162c13a9ff
周伟 <wei.zhou@matrixport.com>
2023-03-04 23:22:34 +0800

    testd ok

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.8.2
 	github.com/zhwei820/gconv v0.0.4
-	github.com/zhwei820/sentry-grpc v0.0.0-20230304150156-ba11cc7c5600
+	github.com/zhwei820/sentry-grpc v0.0.0-20230304152103-28dda841ec65
 	go.uber.org/zap v1.24.0
 	google.golang.org/grpc v1.53.0
 )
--- init_test.go
+++ init_test.go
@@ -5,13 +5,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
 
 func TestInitSentryLogger(t *testing.T) {
 
 	InitSentryLogger("log-01", false, RunModeDebug, 3, "127.0.0.1:8031", "logs/log-01.log")
-	ctx := context.Background()
+	ctx := context.WithValue(context.Background(), TraceID, uuid.NewString())
 
 	for ii := 0; ii < 3; ii++ {
 		ErrorZ(ctx, "info test", zap.String("test", "info"), zap.Int("int", 100), zap.Any("out", map[string]interface{}{"df": 34, "aa": "b"}))
